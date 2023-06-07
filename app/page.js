7f2d80fd7f8653ca260bebf2eef1d45cc923855a
nobledsmarts <nobledsmarts@gmail.com>
2023-06-07 15:32:47 +0100

    update : SearchInput component

--- app/page.js
+++ app/page.js
@@ -1,18 +1,33 @@
+'use client'
+import {  useState } from 'react'
 import { Nav } from '@/components/Nav'
 import Image from 'next/image'
 
 import AppDesc from '@/components/AppDesc'
 import AppHeading from '@/components/AppHeading'
+import { SearchInput } from '@/components/SearchInput';
 
 export default function Home() {
   const appTagline = "A tool that allows you to scrape and download Instagram accounts that matches anything you search.";
+  const [searchValue, setSearchValue] = useState("");
+
+  const onSearchInput = (e) => {
+    setSearchValue(e.target.value);
+  }
   return (
     <main className='container mx-auto'>
         <Nav></Nav>
-          <section className='flex mt-[6rem]'>
+          <section className='flex flex-col mt-[6rem]'>
             <div className='flex justify-center flex-col w-full'>
               <AppHeading firstpart='Insta' secondpart='Search'/>
               <AppDesc content={appTagline} />
+            </div>
+            <div className='flex justify-center'>
+              <SearchInput 
+                placeholder='Search Something...' 
+                value={searchValue} 
+                onInput={onSearchInput}
+              />
             </div>
         </section>
     </main>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
     webpack(config) {
         config.resolve.extensions.push('.ts', '.tsx');
+        // config.module.rules.push({
+        //     test: /\.svg$/i,
+        //     issuer: /\.[jt]sx?$/,
+        //     use: ['@svgr/webpack'],
+        // })
         return config;
       }
 }
