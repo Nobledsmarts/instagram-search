3b3378bbf2c4b967d562180ffecc5811d42b5c9f
nobledsmarts <nobledsmarts@gmail.com>
2023-06-07 16:15:25 +0100

    rename NavLogo.jsx to NavLogo.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Link from 'next/link'
 import { NavButton } from './NavButton'
 import { NavItem } from './NavItem'
+import NavLogo from './NavLogo'
+// import 
 
 export const Nav = () => {
   return (
     <nav className='flex justify-between py-[1.4rem] text-[1rem] leading-[2rem]'>
-        <div>Logo</div>
+        <NavLogo />
         <div className='flex gap-[1rem]'>
             <NavItem />
             <NavButton />
