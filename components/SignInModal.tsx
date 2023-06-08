e8ee822439f3ec11691854883b847f12915d8c23
nobledsmarts <nobledsmarts@gmail.com>
2023-06-08 13:37:49 +0100

    added icons to Sign in modal inputs

--- components/SignInModal.tsx
+++ components/SignInModal.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Modal from './Modal'
 import FormInput from './FormInput'
+import UserIcon from './UserIcon';
+import LockIcon from './LockIcon';
 
 export const SignInModal = () => {
   const [field, setField] = useState({
@@ -14,10 +16,22 @@ export const SignInModal = () => {
 
   return (
     <Modal active={false} title='Login'>
-        <FormInput 
+        <FormInput
+          leftIcon={<UserIcon size='16'/>}
           onInput={changeInputValue} 
-          name='username' type='text' placeholder='Enter Username' value=''/>
-        <FormInput onInput={changeInputValue} name='password' type='password' placeholder='Enter Password' value=''/>
+          name='username' 
+          type='text' 
+          placeholder='Enter Username' 
+          value=''
+        />
+        <FormInput 
+          leftIcon={<LockIcon size='16'/>}
+          onInput={changeInputValue} 
+          name='password' 
+          type='password' 
+          placeholder='Enter Password' 
+          value=''
+        />
     </Modal>
   )
 }
