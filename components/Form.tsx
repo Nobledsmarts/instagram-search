41d548dcb21631ac68cffcc85efb109507c642b6
nobledsmarts <nobledsmarts@gmail.com>
2023-06-08 15:56:13 +0100

    update : modal cancel button

--- components/Form.tsx
+++ components/Form.tsx
@@ -15,7 +15,7 @@ export const Form = (props : FormProps) => {
 
   return (
     <div className='relative rounded-[1rem] bg-dark-blue max-w-[766px] min-w-[400px] p-[1.25rem] flex flex-col justify-center items-center gap-4'>
-      <div className='border-solid border-[2.5px] border-[#596688] absolute flex items-center justify-center w-6 h-6 rounded-full -right-2 -top-2 bg-secondary'>
+      <div className='border-solid overflow-hidden border-[2.5px] border-[#596688] absolute flex items-center justify-center w-7 h-7 hover:cursor-pointer rounded-full -right-2 -top-2 bg-secondary'>
         <CancelIcon size='16' className='bg-secondary'/>
       </div>
       <div className={`flex justify-center text-[#B4C5F0] ${montserrat.className}`}>
